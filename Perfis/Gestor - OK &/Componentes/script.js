f5ff75c8dcb308885ab7686f660654166faae9cb
eduardorsjx7 <ealoisio16@gmail.com>
2024-11-23 01:33:43 -0300

    Add files via upload

---
+++ Perfis/Gestor - OK &/Componentes/script.js
@@ -0,0 +1,32 @@
+// Captura a posição de rolagem
+let scrollX, scrollY;
+
+window.addEventListener('scroll', () => {
+  scrollX = window.scrollX;
+  scrollY = window.scrollY;
+});
+
+// Aplica a função a todos os links <a> para abrir o link na mesma página
+document.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); // Evita o comportamento padrão
+    const url = link.href; // Pega o URL do link
+
+    // Salva a posição de rolagem antes de abrir o novo conteúdo
+    sessionStorage.setItem('scrollX', scrollX);
+    sessionStorage.setItem('scrollY', scrollY);
+
+    // Redireciona para a nova página
+    window.location.href = url;
+  });
+});
+
+// Ao carregar a nova página, restaura a rolagem
+window.addEventListener('load', () => {
+  if (sessionStorage.getItem('scrollX') && sessionStorage.getItem('scrollY')) {
+    window.scrollTo(
+      sessionStorage.getItem('scrollX'),
+      sessionStorage.getItem('scrollY')
+    );
+  }
+});
